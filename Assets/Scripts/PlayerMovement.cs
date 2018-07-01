f57d4e9f39decafc3d1e5fb1eed08f8d632a0772
Sebastian Fülöp <sebastian.fueloep@gmail.com>
2018-07-01 18:30:36 +0200

    Character can jump and move

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    enum JumpState
+    {
+        startJumping,
+        jumping,
+        falling
+    };
+
+    JumpState jumpState = JumpState.falling;
+
+    Rigidbody2D rb;
+
+    SpriteRenderer spriteRenderer;
+
+    float speed = 10;
+
+    float jumpForce;
+    float maxJumpForce = 200;
+    float jumpForceDecrease = 20;
+
+    Vector2 newPosition;
+
+    Quaternion newRotation;
+    
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        newPosition = transform.position;
+        //jumpForce = maxJumpForce;
+
+        newRotation = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0);
+
+        newPosition = transform.position;
+        newPosition.x += Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+        transform.position = newPosition;
+
+
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            newRotation.y = 180;
+            transform.rotation = newRotation;
+        }
+        else if (Input.GetAxis("Horizontal") > 0)
+        {
+            newRotation.y = 0;
+            transform.rotation = newRotation;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("start");
+            jumpForce = maxJumpForce;
+            jumpState = JumpState.startJumping;
+            rb.velocity = new Vector2(0, 0);
+        }
+        
+        if (Input.GetButton("Jump"))
+        {
+            jumpState = JumpState.jumping;
+        }
+
+        
+        if (Input.GetButtonUp("Jump"))
+        {
+            jumpForce = 0;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        switch(jumpState)
+        {
+            case JumpState.jumping:
+                rb.AddForce(new Vector2(0f, jumpForce));
+                jumpForce -= jumpForceDecrease; //or whatever amount
+                
+                if (jumpForce < 0f)
+                    jumpForce = 0f;
+                break;
+        }
+    }
+
+    //private void LateUpdate()
+    //{
+    //    Vector3 position = transform.localPosition;
+
+    //    position.x = (Mathf.Round(transform.parent.position.x * PPU) / PPU) - transform.parent.position.x;
+    //    position.y = (Mathf.Round(transform.parent.position.y * PPU) / PPU) - transform.parent.position.y;
+
+    //    transform.localPosition = position;
+    //}
+}
